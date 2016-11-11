2599204c6be9fafc3dc274c58e6a288ed1a08320
Abigail <adodd591@email.arizona.edu>
2016-11-11 23:10:25 +0000

    updated player hierarchy

--- src/Model/Continents.java
+++ src/Model/Continents.java
@@ -1,5 +1,10 @@
 package Model;
 
+/*
+ * We had a better idea, but we'll change it later! Continents will eventually be objects
+ */
+
+
 public enum Continents {
 	BLUE, GREEN, ORANGE, PINK, RED, YELLOW, BLACK;
 }
--- src/Model/Player.java
+++ src/Model/Player.java
@@ -5,13 +5,23 @@ import java.util.ArrayList;
 public abstract class Player {
 	private String name;
 	private Faction faction;
-	private int lightTroops;
-	private int medTroops;
-	private int heavyTroops;
+	private int availTroops;
 	private ArrayList<Country> myCountries;
 	private ArrayList<Card> myCards;
+//  private Continent[] allContinents; TODO	
 	
+	private void getTroops(){
+		int addedTroops = myCountries.size()/3;
+		//For when the continent class is finished
+		//the added troops changes based on how many
+		//continents you play
+		/*for (Continent con : allContinents){
+			if (this.equals(con.getOccupier()))
+				addedTroops += con.getOwnershipBonus();
+		}*/
+	}//end getTroops
 	
-	
-	
+	public abstract ArrayList<Card> playCards();
+	public abstract Country attack();
+	public abstract void rearrangeTroops();
 }
