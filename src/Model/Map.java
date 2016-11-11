4fe53db6d1a8fb1d9802d56bf12951486cbbcbed
Dylan Tobia <djtobia@gmail.com>
2016-11-11 15:15:36 -0700

    added stuff to map

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -1,5 +1,69 @@
 package Model;
 
 public class Map {
+	
+	private Country countries[] = new Country[50];
+	
+	public Map()
+	{
+		fillCountries();
+	}
+
+	private void fillCountries()
+	{
+		//this method is going to suck
+		countries[0] = new Country();
+		countries[1] = new Country();
+		countries[2] = new Country();
+		countries[3] = new Country();
+		countries[4] = new Country();
+		countries[5] = new Country();
+		countries[6] = new Country();
+		countries[7] = new Country();
+		countries[8] = new Country();
+		countries[9] = new Country();
+		countries[10] = new Country();
+		countries[11] = new Country();
+		countries[12] = new Country();
+		countries[13] = new Country();
+		countries[14] = new Country();
+		countries[15] = new Country();
+		countries[16] = new Country();
+		countries[17] = new Country();
+		countries[18] = new Country();
+		countries[19] = new Country();
+		countries[20] = new Country();
+		countries[21] = new Country();
+		countries[22] = new Country();
+		countries[23] = new Country();
+		countries[24] = new Country();
+		countries[25] = new Country();
+		countries[26] = new Country();
+		countries[27] = new Country();
+		countries[29] = new Country();
+		countries[30] = new Country();
+		countries[31] = new Country();
+		countries[32] = new Country();
+		countries[33] = new Country();
+		countries[34] = new Country();
+		countries[35] = new Country();
+		countries[36] = new Country();
+		countries[37] = new Country();
+		countries[38] = new Country();
+		countries[39] = new Country();
+		countries[40] = new Country();
+		countries[41] = new Country();
+		countries[42] = new Country();
+		countries[43] = new Country();
+		countries[44] = new Country();
+		countries[45] = new Country();
+		countries[46] = new Country();
+		countries[47] = new Country();
+		countries[48] = new Country();
+		countries[49] = new Country();
+	
+	}
+	
+	
 
 }
