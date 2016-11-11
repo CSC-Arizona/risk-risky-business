8950864bd58cdef2b33bcfb2b226eee193110338
Abigail <adodd591@email.arizona.edu>
2016-11-11 21:46:38 +0000

    excel spreadsheet with infos

---
+++ src/Model/Country.java
@@ -0,0 +1,18 @@
+package Model;
+
+import java.util.ArrayList;
+
+/*
+ * 	Authors: 	Dylan Tobia, Abigail Dodd, Sydney Komro, Jewell Finder
+ * 	File:		Country.java
+ * 	Purpose:	Represent a single country
+ */
+
+public class Country {
+	
+	private String name;
+	private double x;
+	private double y;
+	private ArrayList<Country> neighbors;
+
+}
