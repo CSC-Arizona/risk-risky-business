b7e2c1dc85cffdc602b8a46c66c2dd406c8f752e
Jewell <pfinder@Dostoyevsky-3.local>
2016-11-13 11:56:30 -0700

    More tests and HumanPlayer added to hierarchy

---
+++ src/Model/HumanPlayer.java
@@ -0,0 +1,29 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class HumanPlayer extends Player {
+	
+	public HumanPlayer(){ 
+		super();
+	}
+
+	@Override
+	public ArrayList<Card> playCards() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Country attack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void rearrangeTroops() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -5,10 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import Model.Card;
+import Model.Continents;
+import Model.Country;
 import Model.Deck;
+import Model.HumanPlayer;
+import Model.Map;
+import Model.Player;
 
 public class Tests {
-
+	
+	//No tests for Faction or Continents enums
+	
 	@Test
 	public void testArmy() {
 		//There's nothing to test
@@ -16,6 +23,7 @@ public class Tests {
 	
 	@Test 
 	public void testCard(){ 
+		// 100% coverage
 		Card us = new Card("murrica", "infantry");
 		assertEquals(us.getCountry(), "murrica");
 		assertEquals(us.getUnit(), "infantry");
@@ -23,14 +31,41 @@ public class Tests {
 	
 	@Test 
 	public void testDeck(){ 
-		Card us = new Card("murrica", "infantry");
-		Card ca = new Card("canada, eh", "cavalry");
-		
 		Deck testDeck;
 		//need to finish this up
 		
 	}
 	
 	//need tests for Dice
+	@Test
+	public void testCountry(){
+		// 90.6% coverage
+		Country wall = new Country("The Wall", 6.75, 3.5, Continents.BLUE);
+		Country skagos = new Country("Skagos", 10, 3, Continents.BLUE);
+		wall.addNeighbor(skagos);
+		wall.drawMyButton();
+		
+		assertEquals(wall.getName(), "The Wall");
+		assertEquals(wall.getX(), 6.75, 0);
+		assertEquals(wall.getY(), 3.5, 0);
+		assertEquals(wall.getForcesVal(), 0);
+		assertEquals(wall.getOccupier(), null);
+		assertEquals(wall.equals(wall), true);
+	}
+	
+	@Test
+	public void testMap(){
+		// 100% Coverage
+		Map map = new Map();
+		
+		assertEquals(map.getCountries()[0].getName(), "The Wall" );
+	}
+	
+	@Test
+	public void testHumanPlayer(){
+		// 37.5% coverage (Player: 30% Coverage)
+		Player human = new HumanPlayer();
+	}
+	
 	
 }
