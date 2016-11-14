1af9182abb740dd23ee0a5e1350ff02259aaa02b
skomro <skomro@email.arizona.edu>
2016-11-14 12:08:22 -0700

    Filled Deck

--- src/Model/Deck.java
+++ src/Model/Deck.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import Model.Card;
 
 /*
+ * Deck has 53 cards: 51 (one for each territory) + 2 (wild cards)
  * NOTE: Deck class is a singleton, should never have more than one! :) 
  */
 public class Deck {
@@ -17,7 +18,7 @@ public class Deck {
 	private Deck() {
 		riskDeck = new ArrayList<Card>();
 		fillDeck(riskDeck);
-		size = 44;
+		size = 52;
 	}
 
 	public static synchronized Deck getInstance() {
@@ -30,9 +31,10 @@ public class Deck {
 		riskDeck.clear();
 		fillDeck(riskDeck);
 		Collections.shuffle(riskDeck);
-		size = 44;
+		size = 52;
 	}
 
+	//returns null if the deck has run out of cards.
 	public Card deal() {
 		if(size>0){
 			Card result;
@@ -49,10 +51,59 @@ public class Deck {
 		return size;
 	}
 
+	//possible units: infantry, cavalry, artillery
 	private void fillDeck(ArrayList<Card> deck) {
 		// TODO: add all cards to deck
-		deck.add(new Card("Wildlings", "Mammoth")); // example, not sure what
-													// the plan is :)
+		deck.add(new Card("The Wall", "infantry"));
+		deck.add(new Card("Skagos", "cavalry"));
+		deck.add(new Card("Wolfswood", "artillery"));
+		deck.add(new Card("Winterfell", "infantry"));
+		deck.add(new Card("The Rills", "cavalry"));
+		deck.add(new Card("The Neck", "artillery"));
+		deck.add(new Card("The Flint Cliffs", "infantry"));
+		deck.add(new Card("The Grey Cliffs", "cavalry"));
+		deck.add(new Card("The Vale", "artillery"));
+		deck.add(new Card("Riverlands", "infantry"));
+		deck.add(new Card("Iron Islands", "cavalry"));
+		deck.add(new Card("Westerlands", "artillery"));
+		deck.add(new Card("Crownlands", "infantry"));
+		deck.add(new Card("The Reach", "cavalry"));
+		deck.add(new Card("Shield Lands", "artillery"));
+		deck.add(new Card("Whispering Sound", "infantry"));
+		deck.add(new Card("Storm Lands", "cavalry"));
+		deck.add(new Card("Red Mountains", "artillery"));
+		deck.add(new Card("Dorne", "infantry"));
+		deck.add(new Card("Braavosi Coastland", "cavalry"));
+		deck.add(new Card("Andalos", "artillery"));
+		deck.add(new Card("Hills of Norvos", "infantry"));
+		deck.add(new Card("Rhoyne Lands", "cavalry"));
+		deck.add(new Card("Forrest of Qohor", "artillery"));
+		deck.add(new Card("The Golden Fields", "infantry"));
+		deck.add(new Card("The Disputed Lands", "cavalry"));
+		deck.add(new Card("Rhoynian Veld", "artillery"));
+		deck.add(new Card("Sar Mell", "infantry"));
+		deck.add(new Card("Western Waste", "cavalry"));
+		deck.add(new Card("Sea of Sighs", "artillery"));
+		deck.add(new Card("Elyria", "infantry"));
+		deck.add(new Card("Valyria", "cavalry"));
+		deck.add(new Card("Sarnor", "artillery"));
+		deck.add(new Card("Parched Fields", "infantry"));
+		deck.add(new Card("Abandoned Lands", "cavalry"));
+		deck.add(new Card("Western Grass Sea", "artillery"));
+		deck.add(new Card("Kingdoms of the Jfeqevron", "infantry"));
+		deck.add(new Card("Eastern Grass Sea", "cavalry"));
+		deck.add(new Card("The Footprint", "artillery"));
+		deck.add(new Card("Vaes Dothrak", "infantry"));
+		deck.add(new Card("Realms of Jhogrvin", "cavalry"));
+		deck.add(new Card("Ibben", "artillery"));
+		deck.add(new Card("Painted Mountains", "infantry"));
+		deck.add(new Card("Slaver's Bay", "cavalry"));
+		deck.add(new Card("Lhazar", "artillery"));
+		deck.add(new Card("Samyrian Hills", "infantry"));
+		deck.add(new Card("Bayasabhad", "cavalry"));
+		deck.add(new Card("Ghiscar", "artillery"));
+		deck.add(new Card("The Red Waste", "infantry"));
+		deck.add(new Card("Qarth", "cavalry"));
 	}
 
 }
