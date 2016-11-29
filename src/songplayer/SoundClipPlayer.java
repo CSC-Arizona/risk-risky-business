be16bf301e37da2424d0666674d785983598693a
skomro <skomro@email.arizona.edu>
2016-11-28 20:34:51 -0700

    trying so hard

--- src/songplayer/SoundClipPlayer.java
+++ src/songplayer/SoundClipPlayer.java
@@ -23,7 +23,7 @@ public class SoundClipPlayer extends JFrame {
 	public void startTheme() {
 		try {
 			// Open an audio input stream.
-			File soundFile = new File("Game_Of_Thrones_Official_Show_Open_HBO_.wav");
+			File soundFile = new File("The Rains of Castamere (Instrumental.wav");
 			AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
 			// Get a sound clip resource.
 			clip = AudioSystem.getClip();
@@ -54,31 +54,4 @@ public class SoundClipPlayer extends JFrame {
 		clip.stop();
 	}
 
-	public void exitFrame() {
-
-	}
-
-	public void startPlay() {
-		try {
-			// Open an audio input stream.
-			URL url = this.getClass().getClassLoader().getResource("Rains of Castamere instrumental version(1 Hour).");
-			AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
-			// Get a sound clip resource.
-			Clip clip = AudioSystem.getClip();
-			// Open audio clip and load samples from the audio input stream.
-			clip.open(audioIn);
-			// clip.start();
-			clip.loop(Clip.LOOP_CONTINUOUSLY);
-		} catch (UnsupportedAudioFileException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void stopPlay() {
-		clip.stop();
-	}
 }
